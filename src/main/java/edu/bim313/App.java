955dfd815933af6540d606fb4023419a861b0592
okanozdemir1 <okanozdemir@anadolu.edu.tr>
2016-12-26 13:41:25 +0200

    Update App.java

--- src/main/java/edu/bim313/App.java
+++ src/main/java/edu/bim313/App.java
@@ -1,29 +1,30 @@
 package edu.bim313;
+
 import org.paukov.combinatorics3.Generator;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+import static edu.bim313.TurkishNetwork.cities;
+import static edu.bim313.TurkishNetwork.distance;
 
-        ArrayList<Integer> cityList = new ArrayList<>();
-        List<Triangle> triangleList= new ArrayList<>();
-
-        for(int i=0;i<81;i++){
-            cityList.add(i);
+public class App {
+    public static void main(String[] args) {
+        List<List<String>> combinations = Generator.combination(cities).simple(3).stream().collect(Collectors.toList());
+        for (int i = 0; i < combinations.size(); i++) {
+            List<String> control = new ArrayList<>();
+            for (int j = 0; j < 3; j++) {
+                control.add(combinations.get(i).get(j));
+            }
+            int m1 = Arrays.asList(cities).indexOf(control.get(0));
+            int m2 = Arrays.asList(cities).indexOf(control.get(1));
+            int m3 = Arrays.asList(cities).indexOf(control.get(2));
+            if (((distance[m1][m2] + distance[m1][m3]) <= distance[m2][m3]) || ((distance[m1][m2] + distance[m2][m3]) <= distance[m1][m3]) || (((distance[m1][m3] + distance[m2][m3]) <= distance[m1][m2]))) {
+                System.out.println( cities[m1] + " " + cities[m2] + " " + cities[m3]);
+            }
         }
-
-        Generator.combination(cityList)
-                .simple(3)
-                .stream()
-                .forEach((t)->triangleList.add(new Triangle(t)));
-
-        triangleList
-                .stream()
-                .filter(((t) ->t.checkInEquality() ))
-                .forEach(System.out::println);
 
     }
 }
